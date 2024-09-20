8e75c0e6a1fce8da9f668dd63dd320a5cb53b6ff
arqluk <alejandro.lukacher@gmail.com>
2024-09-20 18:19:31 -0300

    Clase6-AL

--- Clase6-AL/api-rest-modulo-http/index.js
+++ Clase6-AL/api-rest-modulo-http/index.js
@@ -2,6 +2,19 @@
 import http from "http"
 import fs from "fs"
 
+// API
+// application programming interface - interfaz de programación de aplicaciones
+// permite la comunicación entre dos componentes de software
+// esta api se basa en un protocolo de solicitudes y respuestas
+
+// API - REST
+// define métodos (verbos) que permiten la comunicación entre el cliente y el servidor
+// EJEMPLO: GET, POST, PUT, PATCH, DELETE
+
+// HTTP
+// protocolo de transferencia de hipertexto
+// es el encargado de la web tal como la conocemos, permite mostrar webs mediante url !
+
 // para realizar una petición http necesitamos:
 // una petición , un método, una respuesta, un encabezado, un código de operación (exdito o error), una url
 
@@ -26,7 +39,8 @@ console.log("REQ: ", url, method)
             res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
             res.end(view)
         } catch (error) {
-            // el bloque catch trae un objeto "error" que dentro va a tener su propio status code 
+            // el bloque catch trae un objeto "error" que dentro va a tener su propio status code
+            //acá puedo elegir de todas formas cómo quiero elaborar esa respuesta en caso de error 
         res.writeHead(404, {"Content-Type": "text/html; charset=utf-8"})
         res.end(error)
         }
@@ -34,7 +48,11 @@ console.log("REQ: ", url, method)
 
 })
 
+
+// Para levantar el servidor
+
 server.listen(8080, () => {
+    //CTRL + click -> sobre la URL y abre automáticamente el navegador predeterminado de tu sistema local
     console.log("Servidor running on http://localhost:8080")
     console.log("Servidor running ... ")
 })
@@ -42,75 +60,3 @@ server.listen(8080, () => {
 server.on("Error", (error) => {
     console.log("Ha ocurrido un error con el servidor. ", error)
 })
-
-    
-    
-   
-
-
-
-
-
-
-
-
-    // if(method === "GET") {
-    //     try {
-    //         const view = await fs.promises.readFile("./public/index.html")
-    //         // Encabezado
-    //         res.writeHead(200, {"Content-Type": "text/html; charset0utf-8"})
-    //         res.end(view)
-    //     } catch (error) {
-    //         // el bloque catch trae un objeto "error" que dentro va a tener su propio status code 
-    //     res.writeHead(404, {"Content-Type": "text/html; charset0utf-8"})
-    //     res.end(error)
-    //     }
-    // }
-
-    
-//})
-
-
-// Para levantar el servidor
-
-// server.listen(8080, () => {
-//     console.log("Servidor running en http://localhost:8080")
-//     console.log("Servidor running ... ")
-// })
-
-// server.on("Error", (error) => {
-//     console.log("Ha ocurrido un error con el servidor. ", error)
-// })
-
-
-
-
-//import fs from "fs"
-
-
-
-
-
-// API
-// application programming interface - interfaz de programación de aplicaciones
-// permite la comunicación entre dos componentes de software
-// esta api se basa en un protocolo de solicitudes y respuestas
-
-// API - REST
-// define métodos (verbos) que permiten la comunicación entre el cliente y el servidor
-// EJEMPLO: GET, POST, PUT, PATCH, DELETE
-
-// HTTP
-// protocolo de transferencia de hipertexto
-// es el encargado de la web tal como la conocemos, permite mostrar webs mediante url !
-// para realizar una petición http necesitamos:
-// una petición , un método, una respuesta, un encabezado, un código de operación, una url
-
-// REQ o REQUEST: objeto de requerimiento
-// RES o RESPONSE: objeto de respuesta
-
-
-//console.log("REQ: ", method)
-
-    // desestructuring object
-    // const { url: url, method: method } = req
